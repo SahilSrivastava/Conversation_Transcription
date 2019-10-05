253830f14d2ae9c5112b4b548557e83f2e760c0c
Sahil Srivastava <sahilsrivastava27@gmail.com>
2019-10-05 13:04:10 +0530

    Committed all files of project

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,10 +1,8 @@
 import pyaudio
 
-#OCM_API = 'c65ff83b1f304484be136ec20c542b04'
-OCM_API = '4e8668c25d584694bad2213865aa9155' #maybe speaker recognistion
+OCM_API = '' #maybe speaker recognistion API
 LOCALE = 'en-us'
-#speech_key, service_region = "cb6f8ab441c949458dd1c9ee295c2da3", "westus"
-speech_key, service_region = "93c66e8e1d374597a1c9d9efd5bf2cd7", "westus" #maybe speech service
+speech_key, service_region = "", "westus" #maybe speech service api
 profile_info={}
 
 FORMAT = pyaudio.paInt16
